d0d00d14a3cada57f04dbe1004b3df0d89fc317d
6704062617342 <s6704062617342@email.kmutnb.ac.th>
2024-11-28 18:26:25 +0700

    1.4

--- lab1minx.c
+++ lab1minx.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 int main(){
 	int n1,n2;
-	printf("");
-	scanf("%d %d",&n1,&n2);
+	printf("n1:\n");
+	scanf("%d",&n1);
+	printf("n2:\n");
+	scanf("%d",&n2);
 	if(n1>n2){
 		printf("n1 is greater than n2\n");
 		printf("%d",n1-n2);
-	}
-	else{
-	printf("n2 is greater than n1\n");
-	printf("%d",n2-n1);	
+	} else{
+		printf("n2 is greater than n1\n");
+		printf("%d",n2-n1);	
 	}
 }
